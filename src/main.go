2bf731484cb5c3bbf83bebcb9bdea71e033e44b1
gabe <gabeklav@fastmail.com>
2024-02-27 00:18:58 -0500

    chore: format

--- src/main.go
+++ src/main.go
@@ -6,61 +6,61 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-    "github.com/joho/godotenv"
-    "github.com/gin-contrib/cors"
+	"github.com/joho/godotenv"
 )
 
 type ListingProps struct {
-    Refs []string `json:"refs"`
-    Price int `json:"price"`
-    Beds float32 `json:"beds"`
-    Baths float32 `json:"baths"`
-    Date string `json:"date"`
-    Notes string `json:"notes"`
-    IsFavorite bool `json:"isFavorite"`
-    IsDismissed bool `json:"isDismissed"`
+	Refs        []string `json:"refs"`
+	Price       int      `json:"price"`
+	Beds        float32  `json:"beds"`
+	Baths       float32  `json:"baths"`
+	Date        string   `json:"date"`
+	Notes       string   `json:"notes"`
+	IsFavorite  bool     `json:"isFavorite"`
+	IsDismissed bool     `json:"isDismissed"`
 }
 
 func basicAuth(c *gin.Context) {
-    user, password, hasAuth := c.Request.BasicAuth()
-    if hasAuth && user == os.Getenv("AUTH_USER") && password == os.Getenv("AUTH_PASS") {
-        c.Next()
-    } else {
-        c.Abort()
-        c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
-        return
-    }
+	user, password, hasAuth := c.Request.BasicAuth()
+	if hasAuth && user == os.Getenv("AUTH_USER") && password == os.Getenv("AUTH_PASS") {
+		c.Next()
+	} else {
+		c.Abort()
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		return
+	}
 }
 
 func getListings(c *gin.Context) {
-    data, err := os.ReadFile("../data/listings.json")
-    if err != nil {
-        log.Print(err)
-    }
+	data, err := os.ReadFile("../data/listings.json")
+	if err != nil {
+		log.Print(err)
+	}
 
-    m := make(map[string]ListingProps)
-    json.Unmarshal(data, &m)
+	m := make(map[string]ListingProps)
+	json.Unmarshal(data, &m)
 
-    c.IndentedJSON(http.StatusOK, m)
+	c.IndentedJSON(http.StatusOK, m)
 }
 
 func ping(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{"pong": true})
+	c.JSON(http.StatusOK, gin.H{"pong": true})
 }
 
 func main() {
-    err := godotenv.Load("../.env")
-    if err != nil {
-        log.Fatalf("Error loading .env: %s", err)
-    }
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatalf("Error loading .env: %s", err)
+	}
 
-    router := gin.Default()
+	router := gin.Default()
 
-    router.Use(cors.Default())
+	router.Use(cors.Default())
 
-    router.GET("/ping", basicAuth, ping)
-    router.GET("/listings",  getListings)
+	router.GET("/ping", basicAuth, ping)
+	router.GET("/listings", getListings)
 
-    router.Run("192.168.88.22:8083")
+	router.Run("192.168.88.22:8083")
 }
