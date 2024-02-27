c8e0789a64dace7ca4a2e365e2856ab3b4a802db
gabe <gabeklav@fastmail.com>
2024-02-26 23:24:58 -0500

    feat(backend): add endpoint for getting listings.json

--- src/go.mod
+++ src/go.mod
@@ -2,7 +2,10 @@ module example/web-service-gin
 
 go 1.21.6
 
-require github.com/gin-gonic/gin v1.9.1
+require (
+	github.com/gin-gonic/gin v1.9.1
+	github.com/joho/godotenv v1.5.1
+)
 
 require (
 	github.com/bytedance/sonic v1.9.1 // indirect
--- src/main.go
+++ src/main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
-    "net/http"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
 
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
+    "github.com/joho/godotenv"
 )
+
+type ListingProps struct {
+    Refs []string `json:"refs"`
+    Price int `json:"price"`
+    Beds int `json:"beds"`
+    Baths int `json:"baths"`
+    Date string `json:"date"`
+    Notes string `json:"notes"`
+    IsFavorite bool `json:"isFavorite"`
+    IsDismissed bool `json:"isDismissed"`
+}
 
 func basicAuth(c *gin.Context) {
     user, password, hasAuth := c.Request.BasicAuth()
-    if hasAuth && user == "testuser" && password == "testpass" {
+    if hasAuth && user == os.Getenv("AUTH_USER") && password == os.Getenv("AUTH_PASS") {
         c.Next()
     } else {
         c.Abort()
@@ -17,13 +32,31 @@ func basicAuth(c *gin.Context) {
     }
 }
 
+func getListings(c *gin.Context) {
+    data, err := os.ReadFile("../data/listings.json")
+    if err != nil {
+        log.Print(err)
+    }
+
+    m := make(map[string]ListingProps)
+    json.Unmarshal(data, &m)
+
+    c.IndentedJSON(http.StatusOK, m)
+}
+
 func ping(c *gin.Context) {
     c.JSON(http.StatusOK, gin.H{"pong": true})
 }
 
 func main() {
+    err := godotenv.Load("../.env")
+    if err != nil {
+        log.Fatalf("Error loading .env: %s", err)
+    }
+
     router := gin.Default()
     router.GET("/ping", basicAuth, ping)
+    router.GET("/listings", getListings)
 
     router.Run("0.0.0.0:8080")
 }
